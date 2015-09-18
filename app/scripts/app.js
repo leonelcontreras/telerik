e6617350dea5996111fd1ac2b8c5edefc9fffcf9
leonel contreras <leonel.contrerasn@gmail.com>
2015-09-18 15:20:34 -0500

    Update app.js
    Se agrego el nuevo mensaje

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,7 @@
     document.addEventListener("deviceready", function () {
         var app = new kendo.mobile.Application(document.body, { skin: "flat" });
         navigator.splashscreen.hide();
-        alert("Hola bebe");
+        alert("Hola como estas?");
+        alert("Todo Bien")
     });
 }());
